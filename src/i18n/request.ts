5167d2aaf055227300859ab0e2d4e6eed0e04269
apollo <1550588466@qq.com>
2025-02-09 09:58:28 +0800

    feat: 调整多语言配置

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,15 +1,24 @@
 import { getRequestConfig } from "next-intl/server";
-import { locales } from "./settings";
-
-export default getRequestConfig(async ({ locale }) => {
-  try {
-    const messages = (await import(`@/messages/${locale}.json`)).default;
-    return {
-      messages,
-    };
-  } catch (error) {
-    return {
-      messages: {},
-    };
+import { routing } from "./routing";
+export default getRequestConfig(async ({ requestLocale }) => {
+  // try {
+  //   const messages = (await import(`@/messages/${locale}.json`)).default;
+  //   return {
+  //     messages,
+  //   };
+  // } catch (error) {
+  //   return {
+  //     messages: {},
+  //   };
+  // }
+  let locale = await requestLocale;
+  console.log(locale, "locale");
+  if (!locale || !routing.locales.includes(locale as any)) {
+    locale = routing.defaultLocale;
   }
+  const messages = (await import(`@/messages/${locale}.json`)).default;
+
+  return {
+    messages,
+  };
 });
